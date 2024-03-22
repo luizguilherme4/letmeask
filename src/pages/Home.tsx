9f1db00b6b1c80246dd124458e627fd1529031f5
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2024-03-21 22:16:35 -0400

    feat(styles): add SCSS styles for homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,11 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss'
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img
           src={illustrationImg}
@@ -15,13 +17,13 @@ export function Home() {
       </aside>
 
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Cria sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
             <input type="text" placeholder="Digite o código da sala" />
             <button type="submit">Entrar na sala</button>

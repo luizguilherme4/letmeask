d89052a02a9baf462d0c191b3f19e5a3b3cbb821
Luiz Guilherme <101076506+luizguilherme4@users.noreply.github.com>
2024-03-22 22:04:11 -0400

    chore(routes): configure react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-// import { Home } from './pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 
 function App() {
-  return <NewRoom />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/rooms/new" Component={NewRoom} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
